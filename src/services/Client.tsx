caa2db598324e92d8278ff2b2a4fa680ef44d567
Evan Kysley <evan@kysley.com>
2019-06-24 16:07:27 -0400

    feat: include token in headers

--- src/services/Client.tsx
+++ src/services/Client.tsx
@@ -3,6 +3,17 @@ import {Provider, createClient} from 'urql'
 
 export const client = createClient({
   url: 'http://138.197.128.14/',
+  fetchOptions: () => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      return {}
+    }
+    return {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  },
 })
 
 export const ClientProvider = ({children}: any) => (
